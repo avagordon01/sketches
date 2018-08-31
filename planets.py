82c044a335f332ae94f8c863fcb423bbd4ab5071
Patrick Gordon <pgpatrickgordon@gmail.com>
2018-08-31 18:45:05 +0100

    planets, show arc travelled, move to A4 size

--- planets.py
+++ planets.py
@@ -2,7 +2,9 @@ import axi
 import math
 import ephem
 
-H, W = axi.A3_SIZE
+A4_SIZE = (11.69, 8.27)
+A4_BOUNDS = (0, 0, 11.69, 8.27)
+H, W = A4_SIZE
 
 def circle(x, y, r, num=60):
     path = []
@@ -28,22 +30,36 @@ def main():
     for n, p in enumerate(planets):
         date = ephem.Date('1995/8/30')
         x, y = pos_at_date(p, date)
+        start_angle = math.atan2(y, x)
+        old_angle = start_angle
         paths.append(circle(x, y, 1000 * radii[n] * 1000 / ephem.meters_per_au))
         path = []
         end_date = ephem.Date('2018/8/30')
+        i = 0
+        round = False
         while date < end_date:
             date += ephem.Date(1)
+            i += 1
             x, y = pos_at_date(p, date)
+            angle = math.atan2(y, x)
+            if math.copysign(1, angle - start_angle) != math.copysign(1, old_angle - start_angle):
+                if not round:
+                    round = True
+                elif round:
+                    path.append(path[0])
+                    break
             path.append((x, y))
+            old_angle = angle
         paths.append(path)
 
     d = axi.Drawing(paths)
 
+    d = d.simplify_paths(0.001)
     d = d.scale_to_fit(W, H)
     d = d.rotate(-90).move(H / 2, W / 2, 0.5, 0.5)
 
     d.dump('out.axi')
-    d.render(bounds=axi.A3_BOUNDS, show_bounds=False).write_to_png('planets.png')
+    d.render(bounds=A4_BOUNDS, show_bounds=False).write_to_png('planets.png')
 
 if __name__ == '__main__':
     main()
